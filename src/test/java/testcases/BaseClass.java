a4e5b4d4fefc428bf9be499264f102954a1f7482
raovba1995 <raovba1995@gmail.com>
2024-10-06 22:21:43 +0530

    added the browser parameter

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -4,12 +4,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 
-
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -18,44 +18,48 @@ import org.testng.annotations.BeforeTest;
 public class BaseClass {
 
 	public static WebDriver driver;
-	
+
 	public XSSFWorkbook wbook;
 	public XSSFSheet sheet;
 
-
-
-	@BeforeMethod(alwaysRun =true)
+	@BeforeMethod(alwaysRun = true)
 	public void SetUpDriver() {
 
-		driver = new ChromeDriver();
+		String browser = System.getProperty("Browser");
+
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			driver = new ChromeDriver();
+		}
+
+		// driver = new ChromeDriver();
 		driver.get("https://simplilearn.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 	}
 
-	@AfterMethod(alwaysRun =true)
+	@AfterMethod(alwaysRun = true)
 	public void TearDown() {
 
 		driver.quit();
 	}
-	
-	@BeforeTest(alwaysRun =true)
+
+	@BeforeTest(alwaysRun = true)
 	public void SetUpExcel() throws IOException {
-		
+
 		FileInputStream fis = new FileInputStream("exceldata.xlsx");
 		wbook = new XSSFWorkbook(fis);
 		sheet = wbook.getSheet("Sheet1");
-		
+
 	}
 
-	@AfterTest(alwaysRun =true)
+	@AfterTest(alwaysRun = true)
 	public void CloseExcel() throws IOException {
-		
+
 		wbook.close();
-		
+
 	}
-
-
 
 }
